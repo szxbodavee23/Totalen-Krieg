0cae10a5455628f8fb4843fdda4952453b277e06
SzDave <szantodavid.mail@gmail.com>
2023-10-10 22:04:19 +0200

    Tovabbi tananyagok hozzaadasa az evszamokhoz

--- tananyagok/evszamok/script.js
+++ tananyagok/evszamok/script.js
@@ -25,7 +25,9 @@ const lis = $all("li");
 
 lis.forEach(li => {
     li.addEventListener("click", (e) => {
-        if (e.target.innerHTML.startsWith("1939")) {
+        let ihtml = e.target.innerHTML;
+        
+        if (ihtml.startsWith("1939")) {
             displayPanel.innerHTML = closeButton +
             "Lengyelország volt az az ország, amelynek területén a Harmadik Birodalom támadásával elkezdődött a második világháború. " +
             "A lengyel állam 123 év után, 1918-ban szerezte vissza a függetlenségét együtt azokkal a földterületekkel, amelyek a 18." +
@@ -36,9 +38,28 @@ lis.forEach(li => {
             "lengyel területet, nem kötöttek békét, így a helyi háború előbb európaivá, azután pedig világháborúvá szélesedett. " +
             "Annak ellenére, hogy Lengyelország a Hitler-ellenes koalícióban, azaz a győztes oldalon állt, a világháború után nem szerezte vissza" + 
             "teljes függetlenségét. Ahogyan a közép-európai államok többsége, úgy Lengyelország is a Szovjetunió fennhatósága alá került." + "<br>" +
-            "<img src='../../pics/lengyel_hadjarat.jpg' style='margin-top: 2rem;'>"; // Itt a kep
-
-            setDisplayPanelVisibility(true);
+            "<img src='../../pics/lengyel_hadjarat.jpg' style='margin-top: 2rem; width: 50%; min-width: 300px;'>"; // Itt a kep
         }
+        else if (ihtml.startsWith("1940. május 10.")) {
+            displayPanel.innerHTML = closeButton + 
+            "A hadjáratot két nagyobb szakaszra lehet osztani: az első szakaszban („Fall Gelb”) a német páncélos ékek átkeltek az Ardennek hegységen " +
+            "és hátulról bekerítették a szövetséges főerőket, amelyek Franciaország északi részén és Belgiumban harcoltak. A harcok eredményeként a " +
+            "Brit Expedíciós Haderő a tengerpartra szorult vissza és a Dinamó hadművelet keretében több mint 300 000 brit, francia és más nemzetiségű " +
+            "katonát kellett kimenteni Dunkerque-ből. A hadjárat következő szakaszában, amely június 5-én kezdődött („Fall Rot”), a német csapatok áttörték " +
+            "a sebtében kiépített francia védelmi állásokat, hátulról megkerülték a fő francia védelmi állást, a Maginot-vonalat, és mélyen benyomultak " +
+            "Franciaország területére. Június 10-én Olaszország is hadat üzent Franciaországnak és támadást indított az ország déli részén." + 
+            "<img src='../../pics/parizs_terkep.jpg' style='margin-top: 2rem; width: 50%; min-width: 300px;'>";
+        }
+        else if (ihtml.startsWith("1940. június 22.")) {
+            displayPanel.innerHTML = closeButton +
+            "A hadjárat a tengelyhatalmak döntő győzelmével zárult: Franciaországot két, északi és nyugati megszállási zónára osztották fel, " +
+            "délen az olasz csapatok tartottak megszállva egy kis részt, míg a maradékon – az ún. zone libre – megalakult a Vichy-kormány, " +
+            "amely elvileg polgári fennhatóságot gyakorolt mindhárom zónában. 1942 novemberében a német csapatok megszállták a zone libre-t is, " +
+            "és Franciaország német megszállás alatt maradt egészen 1944 júniusáig, amikor a szövetségesek partra szálltak Normandiában. " +
+            "Franciaország területének nagy része csak 1944 szeptemberére szabadult fel." + "<br>" +
+            "<img src='../../pics/francia_hadjarat.jpg' style='margin-top: 2rem; width: 50%; min-width: 300px;'>";
+        }
+
+        setDisplayPanelVisibility(true);
     });
 });
