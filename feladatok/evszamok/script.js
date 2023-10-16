60be65e01a55543397859aadaf744389dd289736
David Szanto <szantodavid.mail@gmail.com>
2023-10-16 10:43:02 +0200

    Alairat atbugolasanak kijavitasa

--- feladatok/evszamok/script.js
+++ feladatok/evszamok/script.js
@@ -19,13 +19,16 @@ const setDisplayPanelVisibility = (visibility) => {
         displayPanel.style = "display: block; position: fixed; left: 50%; top: 50%; transform: translate(-50%, -50%); background-color: gray;" +
             "padding: 1rem; border-radius: 25px; box-shadow: 10px 10px 15px black;";
         $(".link-back").style = "display: none";
+        $(".alairat").style = "display: none";
     }
     else if (visibility && window.innerHeight < 1000) {
         displayPanel.style = "display: block; position: absolute; left: 50%; top: 0%; transform: translate(-50%, 0%); background-color: gray;" +
             "padding: 1rem; border-radius: 25px; box-shadow: 10px 10px 15px black;";
         $(".link-back").style = "display: none";
+        $(".alairat").style = "display: none";
     }
     else if (!visibility) { 
+        $(".alairat").style = "display: block";
         displayPanel.style = "display: none;";
         $(".link-back").style = "display: inline";
 
